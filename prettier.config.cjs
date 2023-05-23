3f31227b177f5b6f79e28d824788b0d2bfbe516f
davud <davudgorqi@gmail.com>
2023-05-23 11:18:46 +0200

    feat: home page styling and layout

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  trailingComma: "es5",
+  tabWidth: 4,
 };
 
 module.exports = config;
